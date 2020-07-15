ea142bf9b19365cada4c0ed914ae41fbf4bd635c
unknown <gyebnarpetter@gmail.com>
2020-07-15 14:26:19 +0200

    test

--- script.js
+++ script.js
@@ -1,94 +1,32 @@
-let imagesData = [
-    {
-        photo: "img/pencil.jpg",
-        title: "Colored pencils",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-        photo: "img/cassette.jpg",
-        title: "Old school cassette",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-        photo: "img/cat.jpg",
-        title: "Cute cat",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-        photo: "img/chameleon.jpg",
-        title: "Chameleon from the jungle",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-        photo: "img/house.jpg",
-        title: "Pretty houses",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-        photo: "img/lamp.jpg",
-        title: "Lamps in the night",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+/* MOBILE MENU TOGGLE
+------------------------------------------------------------------------ */
+
+$(".nav-icon").click(() => {
+    $("header").slideToggle();
+});
+
+
+/* SLIDER
+------------------------------------------------------------------------ */
+
+let imagesIndex = 0;
+let i = 0;
+let images = document.getElementsByClassName('slider-img');
+
+let slider = () => {
+
+    for ( i = 0; i < images.length; ++i ) {
+        images[i].style.display = 'none';
     }
-];
 
+    if ( imagesIndex === images.length ) {
+        imagesIndex = 0;
+    }
 
-// FIRST IMAGE
-let currentPhoto = 0;
+    images[imagesIndex].style.display = 'block';
+    imagesIndex++;
 
-let loadPhoto = (photoNumber) => {
-    $("#photo").attr("src", imagesData[photoNumber].photo);
-    $("h3").text(imagesData[photoNumber].title);
-    $("p").text(imagesData[photoNumber].description);
-};
+    setTimeout(slider, 5000);
+}
 
-loadPhoto(currentPhoto);
-
-//NEXT IMAGE
-$("#right-arrow").click(() => {
-    currentPhoto < 6 ? currentPhoto++ : currentPhoto = 0;
-    loadPhoto(currentPhoto);
-});
-
-//PREVIOUS IMAGE
-$("#left-arrow").click(() => {
-    currentPhoto > 0 ? currentPhoto-- : currentPhoto = 6;
-    loadPhoto(currentPhoto);
-});
-
-//THUMBNAILS
-let thumbnailsId = 0;
-imagesData.forEach((item) => {
-    $("#thumbnails-wrapper").append(`<img id="thumbnails${thumbnailsId}">`);
-    $("#thumbnails" + thumbnailsId).attr("src", item.photo);
-    ++thumbnailsId;
-});
-
-$("#thumbnails0").click(() => {
-    currentPhoto = 0;
-    loadPhoto(currentPhoto);
-});
-
-$("#thumbnails1").click(() => {
-    currentPhoto = 1;
-    loadPhoto(currentPhoto);
-});
-
-$("#thumbnails2").click(() => {
-    currentPhoto = 2;
-    loadPhoto(currentPhoto);
-});
-
-$("#thumbnails3").click(() => {
-    currentPhoto = 3;
-    loadPhoto(currentPhoto);
-});
-
-$("#thumbnails4").click(() => {
-    currentPhoto = 4;
-    loadPhoto(currentPhoto);
-});
-
-$("#thumbnails5").click(() => {
-    currentPhoto = 5;
-    loadPhoto(currentPhoto);
-});
+slider();
